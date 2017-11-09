0aeb339e465c77d174aaf1ad333eb692c072a6c3
Navraj Saini <saini4@36flash.cs.uleth.ca>
2017-11-08 21:12:03 -0700

    added the .h file... I don't know if we wanna use it

---
+++ Splay.h
@@ -0,0 +1,15 @@
+//**********************************
+// Splay tree .h file
+//**********************************
+
+#include <iostream>
+#include <functional>
+
+#ifndef SPLAY_H
+#define SPLAY_H
+
+class splay_t
+{
+  private:
+   
+}:
